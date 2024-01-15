0816601bf8aaf0c03f6f3aba4f3c8f6dfd145ee7
arifkhan101 <arifkhan230122@gmail.com>
2024-01-15 14:17:32 +0600

    added counterReducer function

--- redux-tutorial-2024/index.js
+++ redux-tutorial-2024/index.js
@@ -1,14 +1,10 @@
 // Defining constants
 const INCREMENT = "INCREMENT"
 const DECREMENT = "DECREMENT"
-const ADD_USER = "ADD_USER"
 // state 
 
 const initialCounterState = {
     count: 0
-}
-const initialUsersState = {
-    users: [{ name: "Arif khan" }]
 }
 
 // action => Object => type, payload
@@ -23,12 +19,27 @@ const decrementCounter = () => {
         type: DECREMENT,
     }
 }
-const addUser = () => {
-    return {
-        type: ADD_USER,
-        payload:{name: "shakil"}
+
+// create reducer for counter
+const counterReducer = (state = initialCounterState, action) => {
+
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+
+        default:
+            state;
     }
 }
+
 
 
 // 1. action
