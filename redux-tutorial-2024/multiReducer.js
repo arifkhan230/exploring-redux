7bd9f157770e6625aa208e3ac73345f36bc63790
arifkhan101 <arifkhan230122@gmail.com>
2024-01-18 20:39:00 +0600

    explored middleware

--- redux-tutorial-2024/multiReducer.js
+++ redux-tutorial-2024/multiReducer.js
@@ -1,23 +1,17 @@
-const { createStore, combineReducers } = require("redux")
+const { createStore, applyMiddleware } = require("redux")
+const { default: logger } = require("redux-logger")
 
 // products Constants
 const GET_PRODUCTS = "GET_PRODUCTS"
 const ADD_PRODUCT = "ADD_PRODUCT "
 
-// cart Constants
-const GET_CART_ITEMS = "GET_CART_ITEMS"
-const ADD_CART_ITEMS = "ADD_CART_ITEMS "
 
 // PRODUCTS STATES
 const initialProductState = {
     products: ["sugar", "salt"],
     numberOfProducts: 2,
 }
-// CARTS STATES
-const initialCartState = {
-    cart: ["sugar", ],
-    numberOfProducts: 1,
-}
+
 
 // PRODUCTS ACTIONS
 const getProducts = () => {
@@ -32,19 +26,7 @@ const addProduct = (product) => {
         payload: product,
     }
 }
-// CARTS ACTIONS
-const getCart = () => {
-    return {
-        type: GET_CART_ITEMS,
-    }
-}
 
-const addCart = (product) => {
-    return {
-        type: ADD_CART_ITEMS,
-        payload: product,
-    }
-}
 
 // Product reducer
 const productReducer = (state = initialProductState, action) => {
@@ -65,39 +47,15 @@ const productReducer = (state = initialProductState, action) => {
            return state;
     }
 }
-// cart reducer
-const cartReducer = (state = initialCartState, action) => {
-    switch (action.type) {
-        case GET_CART_ITEMS:
 
-            return {
-                ...state
-            }
-        case ADD_CART_ITEMS:
 
-            return {
-                cart: [...state.cart, action.payload],
-                numberOfProducts: state.numberOfProducts + 1
-            }
 
-        default:
-           return state;
-    }
-}
 
-// combining reducer 
-const rootReducer = combineReducers({
-    productR: productReducer,
-    cartR: cartReducer
-})
-
-const store = createStore(rootReducer);
+const store = createStore(productReducer,applyMiddleware(logger));
 store.subscribe(()=>{
     console.log(store.getState());
 })
 
-store.dispatch(getCart())
-store.dispatch(getProducts())
 
-store.dispatch(addCart("milk"))
+store.dispatch(getProducts())
 store.dispatch(addProduct("tea"))
